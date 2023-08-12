401e669677ef6f1af25d35845254dc11a1e8d304
Bryan Ramos <bryan@ramos.codes>
2023-08-12 14:22:13 -0400

    Moved users from file to service

--- src/system/modules/system/users.nix
+++ src/system/modules/system/users.nix
@@ -12,23 +12,5 @@
       extraGroups = [ "wheel" "bitcoin" ];
       initialPassword = "satoshi";
     };
-
-    core = {
-      isSystemUser = true;
-      extraGroups = [ "bitcoin" ];
-      shell = "/usr/bin/false";
-    };
-
-    electrum = {
-      isSystemUser = true;
-      extraGroups = [ "bitcoin" ];
-      shell = "/usr/bin/false";
-    };
-
-    lightning = {
-      isSystemUser = true;
-      extraGroups = [ "bitcoin" ];
-      shell = "/usr/bin/false";
-    };
   };
 }
