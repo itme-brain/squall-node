fcdab4119e216e6fd6619c62d507aab5611e291c
Bryan Ramos <bryan@ramos.codes>
2023-08-12 14:34:23 -0400

    Todo

--- src/system/default.nix
+++ src/system/default.nix
@@ -5,6 +5,8 @@
     ./modules/.
   ];
 
+# TODO: Move the services from system level to user level, add home-manager and change user file to user directory
+
   nix = {
     packages = pkgs.nixFlakes;
     extraOptions = "experimental-features = nix-command flakes";
