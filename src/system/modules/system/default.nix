9b3b1d538e58c639c745d97b824ea56eec54ede1
Bryan Ramos <bryan@ramos.codes>
2023-08-13 10:06:58 -0400

    Added

--- src/system/modules/system/default.nix
+++ src/system/modules/system/default.nix
@@ -1,6 +1,17 @@
+{ pkgs, ... }:
+
 { import = [
     ./locale.nix
     ./network.nix
     ./users.nix
   ];
+
+  nix = {
+    package = pkgs.nixFlakes;
+    extraOptions = "experimental-features = nix-command flakes";
+    settings = {
+      auto-optimise-store = true;
+      trusted-users = [ "bryan" ];
+    };
+  };
 }
