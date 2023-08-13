73eb7c16afd3102d3e5a4e6dba40cabb4fafdab2
Bryan Ramos <bryan@ramos.codes>
2023-08-13 10:07:23 -0400

    Added correct outputs

--- flake.nix
+++ flake.nix
@@ -1,17 +1,29 @@
 {
-  description = "Squall - Plug-n-Play Bitcoin Node";
+  description = "Squall - Bitcoin Node";
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem
-    (system:
-    let
-      pkgs = import nixpkgs { inherit system; };
+  flake-utils.lib.eachDefaultSystem
+  (system:
+  let
+    pkgs = import nixpkgs { inherit system; };
 
-    in with pkgs;
-    {
-      devShells.default = callPackage ./shell.nix { };
+  in with pkgs;
+  {
+    nixosConfigurations.node = nixpkgs.lib.nixosSystem {
+      inherit system pkgs;
+      modules = [ ./system/modules/system
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.squall = import ./system/modules/home;
+        }
+      ];
+    };
+
+    devShells.default = callPackage ./shell.nix { };
   });
 }
