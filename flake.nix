8fca80570883ccf469dd59b62317294259ad0c91
Bryan Ramos <bryan@ramos.codes>
2024-01-26 16:58:48 -0500

    Updated flake

--- flake.nix
+++ flake.nix
@@ -1,14 +1,22 @@
 {
   description = "Squall - Bitcoin Node";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs =
+  {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    home-manager= {
+      url = "github:nix-community/home-manager/release-23.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
 
   outputs = { self, nixpkgs, flake-utils, home-manager}:
   flake-utils.lib.eachDefaultSystem
   (system:
   let
-    pkgs = import nixpkgs { inherit system; };
+    pkgs = import nixpkgs {
+      inherit system;
+    };
 
 
   in with pkgs;
